7350335750cca0c322ba46631f3c9c26adaae10b
lowe21 <78665806@qq.com>
2024-11-14 15:07:56 +0800

    搭建基本布局

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,26 @@
 </script>
 
 <template>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <el-container>
+    <el-main>
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    </el-main>
+    <el-aside></el-aside>
+  </el-container>
 </template>
 
 <style scoped>
-header {
+.el-container {
+  height: 100vh;
   line-height: 1.5;
+}
+
+.el-main {
+  background-color: green;
+}
+
+.el-aside {
+  background-color: purple;
 }
 
 .logo {
