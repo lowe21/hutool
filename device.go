efeea3ca0380df182e8294fd3b0a7cd7baa27648
lowe21 <78665806@qq.com>
2024-11-27 19:12:06 +0800

    采集传感器数据

--- device.go
+++ device.go
@@ -19,15 +19,49 @@ func (device *Device) listener() {
 	if err != nil {
 		websocket.Notice(websocket.Message("error", "DEVICE_ERROR", err.Error()))
 	} else {
+		count := 0
+		data := make([]map[string]any, 0, 10)
+
 		for {
-			data := make([]byte, 128)
-			n, err := port.Read(data)
+			buffer := make([]byte, 128)
+			n, err := port.Read(buffer)
 			if err != nil {
 				websocket.Notice(websocket.Message("error", "DEVICE_ERROR", err.Error()))
 				break
 			}
-			fmt.Printf("%X\n", data[:n])
-			fmt.Printf("%v\n", data[:n])
+			if n != 14 {
+				continue
+			}
+
+			var (
+				header string
+				footer string
+			)
+			for _, value := range data[:2] {
+				header += fmt.Sprintf("%X", value)
+			}
+			for _, value := range data[n-2:] {
+				footer += fmt.Sprintf("%X", value)
+			}
+			if header != "FF81" || footer != "CC5A" {
+				continue
+			}
+
+			for index, value := range buffer[2 : n-2] {
+				data = append(data, map[string]any{
+					"line": index,
+					"x":    count,
+					"y":    value,
+				})
+			}
+
+			count++
+
+			if count >= 10 {
+				websocket.Notice(websocket.Message("error", data))
+				count = 0
+				data = data[:0]
+			}
 		}
 	}
 
